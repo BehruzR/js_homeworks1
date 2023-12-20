a7754762854bd4153ce33f2a6b8fd5ea97e1eeb6
Behrus <Behrus@gmial.com>
2023-12-20 13:20:15 +0500

    ferst commit

---
+++ app.js
@@ -0,0 +1,33 @@
+var imya = prompt("Введите своё имя");
+
+console.log("Меня зовут " + imya);
+
+var age = prompt('Введите свой возраст');
+
+console.log("Мне " + age);
+
+var plus = prompt('Решите пример 6 + 3 = ?');
+
+console.log("Пример 1: 6 + 3 = " + (6 + 3) + ' Ваш ответ ' + plus);
+
+var multi = prompt('Решите пример 15 * 2 = ?');
+
+console.log("Пример 2: 15 * 2 = " + (15 * 2) + ' Ваш ответ ' + multi);
+
+var devi = prompt('Решите пример 8 / 4 = ?');
+
+console.log("Пример 3: 8 / 4 = " + (8 / 4) + ' Ваш ответ ' + devi);
+
+var proc = prompt('Решите пример 10 % 3 = ?');
+
+console.log("Пример 4: 10 % 3 = " + (10 % 3) + ' Ваш ответ ' + proc);
+
+var firstEnd = prompt('Откройте консоль для просмотра');
+
+var first = +prompt('Введите первое число');
+
+var second = +prompt('Введите второе число');
+
+var third = +prompt('Введите третье число');
+
+alert('Среднее арифметическое: ' + (first + second + third) / 3);
